db65a0b5ff8b9c0a45894f0d6e177ed3d0a58015
VM <48353237+1hemays@users.noreply.github.com>
2021-09-20 03:15:52 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Artyom Ma  on 21.07.2021.
+//
+
+import UIKit
+import SwiftUI
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) /* #8997f4 */
+//        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(change), userInfo: nil, repeats: false)
+        
+
+
+
+
+}
+
+
+}
